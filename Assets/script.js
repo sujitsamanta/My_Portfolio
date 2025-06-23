f3dd8cfb2b06aa4d2b3a6813fcd12d0282790e80
sujitsamanta <143060926+sujitsamanta@users.noreply.github.com>
2025-06-23 20:35:39 +0530

    Add Message Receive Logic

--- Assets/script.js
+++ Assets/script.js
@@ -53,6 +53,10 @@
    e.stopPropagation();
  });
 
+
+
+
+
  // Smooth scrolling for navigation links
  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
    anchor.addEventListener("click", function (e) {
@@ -156,7 +160,7 @@
 
 
 
- // Tab switching functionality
+ // Education and Wark section Tab switching functionality
  const workTab = document.getElementById('workTab');
  const educationTab = document.getElementById('educationTab');
  const workSection = document.getElementById('workSection');
@@ -181,4 +185,48 @@
      educationSection.classList.remove('hidden');
      workSection.classList.add('hidden');
  });
+
+
+
+    
+// contact-us receive message
+const form = document.getElementById('form');
+const result = document.getElementById('result');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const object = Object.fromEntries(formData);
+  const json = JSON.stringify(object);
+  result.innerHTML = "Please wait..."
+
+    fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: json
+        })
+        .then(async (response) => {
+            let json = await response.json();
+            if (response.status == 200) {
+                result.innerHTML = "Form submitted successfully";
+            } else {
+                console.log(response);
+                result.innerHTML = json.message;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            result.innerHTML = "Something went wrong!";
+        })
+        .then(function() {
+            form.reset();
+            setTimeout(() => {
+                result.style.display = "none";
+            }, 3000);
+        });
+});
+
 
